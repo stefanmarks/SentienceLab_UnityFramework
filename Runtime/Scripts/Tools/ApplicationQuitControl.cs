78bb02b3a5ce4e7c7e099033ed49c42f1e5e02d6
Stefan Marks <smarks@aut.ac.nz>
2020-11-13 19:11:37 +1300

    Removing old Input code from ApplicationQuitControl

--- Runtime/Scripts/Tools/ApplicationQuitControl.cs
+++ Runtime/Scripts/Tools/ApplicationQuitControl.cs
@@ -1,12 +1,35 @@
-﻿using UnityEngine;
+﻿#region Copyright Information
+// Sentience Lab - Unity Framework
+// (C) Sentience Lab (sentiencelab@aut.ac.nz), Auckland University of Technology, Auckland, New Zealand 
+#endregion Copyright Information
+
+using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class ApplicationQuitControl: MonoBehaviour
 {
-	public void Update()
+	[Tooltip("action that quits the application (default: ESC)")]
+	public InputAction QuitAction = null;
+	
+	public void Start()
 	{
-		if (Input.GetKeyDown(KeyCode.Escape))
+		if (QuitAction == null)
 		{
-			Application.Quit();
+			QuitAction = new InputAction(binding: "<Keyboard>/escape");
 		}
+
+		QuitAction.performed += delegate { Quit(); };
+		QuitAction.Enable();
+	}
+
+
+	public void Quit()
+	{
+		Debug.Log("Quitting application");
+#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+#else
+		Application.Quit();
+#endif
 	}
 }
